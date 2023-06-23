d86fdcfcf7be86e686b81a73195cb71bd8a92118
Noman <noman92786@gmail.com>
2023-06-23 11:13:32 +0400

    initial setup

---
+++ src/test/java/com/example/NagarroAssignment/NagarroAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NagarroAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagarroAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
